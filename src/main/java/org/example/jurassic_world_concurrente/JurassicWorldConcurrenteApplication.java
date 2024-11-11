01f9026519a08568ed4b910cf01d0f2f2f221786
ffernandoss <fsantama@myuax.com>
2024-11-11 17:02:39 +0100

    dinosaurios roar

---
+++ src/main/java/org/example/jurassic_world_concurrente/JurassicWorldConcurrenteApplication.java
@@ -0,0 +1,13 @@
+package org.example.jurassic_world_concurrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicWorldConcurrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicWorldConcurrenteApplication.class, args);
+	}
+
+}
