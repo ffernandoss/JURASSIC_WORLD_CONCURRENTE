32da909ce4f09dde10dffda4ac1d6f06f73e28fd
gcorona72 <oyono1996@gmail.com>
2024-11-16 15:22:41 +0100

    Informacion impresa 1

---
+++ src/main/java/org/example/jurassic_world_concurrente/BrowserLauncher.java
@@ -0,0 +1,28 @@
+package org.example.jurassic_world_concurrente;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class BrowserLauncher implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(BrowserLauncher.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        String url = "http://localhost:8080/index.html"; // URL de la página index
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("open", url);
+            processBuilder.start();
+            logger.info("Navegador abierto con la URL: {}", url);
+        } catch (IOException e) {
+            logger.error("Error al abrir el navegador: ", e);
+        }
+    }
+}
